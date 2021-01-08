2f34ac0090b2eb3ac799a9d80327d6481cda5427
Chris Staykov <cstaykov@uwaterloo.ca>
2021-01-08 17:12:24 -0500

    Add company photos

--- src/components/elements/Timeline.js
+++ src/components/elements/Timeline.js
@@ -3,6 +3,11 @@ import TimelineItem from "./TimelineItem";
 import Resume from "../../resume.json";
 
 function Timeline() {
+  let years= new Set();  
+  Resume.work.forEach(item => {
+    years.add(new Date(item.startDate).getFullYear())
+  });
+  let YearsList = [...years];
   return (
     <div className="timeline is-centered">
       <header className="timeline-header">
@@ -14,11 +19,7 @@ function Timeline() {
         <div className="timeline-marker is-success"></div>
         <div className="timeline-content"></div>
       </div>
-      {Resume.work
-        .map(item => {
-          return new Date(item.startDate).getFullYear();
-        })
-        .map((year, i) => {
+      {YearsList.map((year, i) => {
           let content = [];
           content.push(
             <header key={i} className="timeline-header">
@@ -38,6 +39,8 @@ function Timeline() {
                     })}
                     company={item.company}
                     summary={item.summary}
+                    picture={item.picture}
+                    website={item.website}
                   />
                 );
               })
--- src/components/elements/TimelineItem.js
+++ src/components/elements/TimelineItem.js
@@ -3,9 +3,9 @@ import React from "react";
 function TimelineItem(props) {
   return (
     <div className="timeline-item is-success">
-      <div className="timeline-marker is-image is-32x32">
-        <img src="" alt="" />
-      </div>
+      <a className="timeline-marker is-image is-32x32" href={props.website} target="_blank" rel="noopener noreferrer">
+        <img src={props.picture} alt="" />
+      </a>
       <div className="timeline-content">
         <p className="heading">{props.date}</p>
         <h1 className="title is-4">{props.company}</h1>
